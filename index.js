4caaf2a78bbf0650f6b681b68b6f9daa4bd7ba9c
Timimah <44163985+Timimah@users.noreply.github.com>
2022-09-29 11:46:14 +0100

    made a few adjustments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ getTable = () => {
         eval_table.innerHTML += `<div class="w-100 border-dark">`
         for( let j = 1; j <= noOfColumns.value; j++)
         if(evaluate.value == 'Add')  {
-            eval_table.innerHTML += `<div class="col border text-center p-3">${j} + ${i} = ${(j + i)}</div><br><br>`
+            eval_table.innerHTML += `<div class="col border text-center py-3">${j} + ${i} = ${(j + i)}</div><br><br>`
         }
         else if (evaluate.value == 'Multiply'){
             eval_table.innerHTML += `<div class="col border text-center p-3">${j} x ${i} = ${(j * i)}</div><br><br>`
