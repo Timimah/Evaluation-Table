44f733b6c251c79428f30aceb919835a03df1830
Timimah <44163985+Timimah@users.noreply.github.com>
2022-09-29 03:28:39 +0100

    removed table and used div

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 getTable = () => {
-    alert()
+    // alert()
     for (let i = 1; i <= noOfRows.value; i++) {
-        console.log(i)
-        // eval_table.innerHTML += `<tr></tr>`
+        eval_table.innerHTML += `<div class="w-100 border-dark">`
         for( let j = 1; j <= noOfColumns.value; j++)
-        // console.log(i*j)
         if(evaluate.value == 'Add')  {
-            // eval_table.innerHTML += `<tr></tr>`
-            eval_table.innerHTML += `<td>${i} + ${j} = ${(i + j)}</td>`
-            // eval_table.innerHTML += `</tr>`
+            eval_table.innerHTML += `<div class="col border text-center p-3">${j} + ${i} = ${(j + i)}</div><br><br>`
         }
         else if (evaluate.value == 'Multiply'){
-            eval_table.innerHTML = `${i} x ${j} = ${(i * j)}`
-            // eval_table.innerHTML += `</tr>`
+            eval_table.innerHTML += `<div class="col border text-center p-3">${j} x ${i} = ${(j * i)}</div><br><br>`
         }
         else if (evaluate.value == 'Divide'){
-            eval_table.innerHTML += `${i} / ${j} = ${(i / j)}`
-            // eval_table.innerHTML += `</tr>`
+            eval_table.innerHTML += `<div class="col border text-center p-3">${j} / ${i} = ${(j / i)}</div><br><br>`
         }
-        else if (evaluate.value == 'Subtract'){
-            eval_table.innerHTML += `${i} - ${j} = ${(i - j)}`
-            
+        else if (evaluate.value == 'Subtdact'){
+            eval_table.innerHTML += `<div class="col border text-center p-3">${j} - ${i} = ${(j - i)}</div><br><br>`
         }
-        // eval_table.innerHTML += `</tr></tr>`
+        eval_table.innerHTML += `</div>`
     }
 }
